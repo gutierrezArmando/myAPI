12531b61255b6dcade689ca89c2a55c36d3b9b1a
Armando Gutiérrez <gutierrezjarr@gmail.com>
2018-05-19 22:39:38 -0800

    consulta de juzgado

--- src/rutas/despacho/juzgados.js
+++ src/rutas/despacho/juzgados.js
@@ -26,4 +26,18 @@ app.get('/partidos', function (req, res) {
     })
 });
 
+/*Para desplegar el juzgado de acuerdo al partidojudicial y al tipo de materia civil*/
+app.get('/juzgado/:materia/:partido', function (req, res) {
+    var datos = req.params;
+    var consulta = 'select id, numero from juzgados where idMateria = ? and idPartidoJudicial = ?';
+    conection.query(consulta,[datos.materia, datos.partido] ,function (error, result, fields) {
+        if(error)
+            throw error;
+        //return res.json({error: false, data: result, message: 'Lista de materias'});
+        return res.json(result);
+        res.status(404);
+        res.send("Error al buscar en base de datos");
+    })
+});
+
 module.exports = app;
